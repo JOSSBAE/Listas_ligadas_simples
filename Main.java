29a093516f847d4cfa3c9c3a9b7bf6db9c1c8b82
JOSSBAE <148731691+JOSSBAE@users.noreply.github.com>
2023-12-02 00:38:13 -0500

    Actualizar clase main

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        lista.add("A");
+        lista.add("B");
+        lista.add("C");
+        lista.add("D");
+        lista.add("E");
+        System.out.println("Lista original: ");
+        System.out.println(lista);
+        lista.swap("A", "E");
+        System.out.println("Lista con A y E intercambiados: ");
+        System.out.println(lista);
+        lista.swap("B", "D");
+        System.out.println("Lista con B y D intercambiados: ");
+        System.out.println(lista);
+        lista.swap("A", "C");
+        System.out.println("Lista con A y C intercambiados: ");
+        System.out.println(lista);
+        System.out.println("Buscando por posición: ");
+        System.out.println(lista.search(0).data);
+        System.out.println(lista.search(1).data);
+        System.out.println(lista.search(2).data);
+        System.out.println(lista.search(3).data);
+        System.out.println(lista.search(4).data);
+    }
+}
